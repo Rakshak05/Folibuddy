765301afb1067963335f2878e51cebc5bdfe7ac0
Rakshak S. Barkur <159248180+Rakshak05@users.noreply.github.com>
2026-01-24 17:15:24 +0530

    Refactor run.py to import os and set port from env

--- run.py
+++ run.py
@@ -1,13 +1,14 @@
 import sys
+import os
+import uvicorn
 from pathlib import Path
 
 # Force project root into PYTHONPATH
 ROOT = Path(__file__).resolve().parent
 sys.path.insert(0, str(ROOT))
 
-import uvicorn
-
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(
         "main:app", 
         host="0.0.0.0",
