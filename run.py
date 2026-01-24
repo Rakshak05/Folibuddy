a3094b5152f1f7418576a31d3043d6019be831e2
Rakshak S. Barkur <159248180+Rakshak05@users.noreply.github.com>
2026-01-24 17:19:09 +0530

    Update run.py

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ sys.path.insert(0, str(ROOT))
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(
-        "main:app", 
+        "app:app", 
         host="0.0.0.0",
         port=port,
         reload=False 
